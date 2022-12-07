c701f71be7086433dc01c05fa4cb62eeab40fcae
Mrh <m@mrhsystems.com>
2022-12-07 12:47:15 +1100

    initial upload.

--- Module1.vb
+++ Module1.vb
@@ -1,7 +1,14 @@
-﻿Module Module1
+﻿Imports Lib3CXPhone
+
+Module Module1
 
     Sub Main()
+        Dim LaunchArg As String = Chr(34) & "example" & Chr(34) & " " & Chr(34) & "%CallerNumber%" & Chr(34)
+
+        Lib3CXPhone.Lib3CXPhone.Set3CXLaunchProgram("C:\Scrips\action-client.exe", LaunchArg)
 
     End Sub
+
+
 
 End Module
